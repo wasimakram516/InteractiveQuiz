3df5bfc7f071da8a741ba3c958d40c28374696f9
Wasim Akram <mr.saim516@gmail.com>
2024-10-31 16:43:36 +0500

    created android app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.initeractiveQuiz.apk',
+  appName: 'Interactive Quiz',
+  webDir: 'build'
+};
+
+export default config;
